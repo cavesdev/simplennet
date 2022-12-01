fix: works!  added error calculation  print accuracy

main.py
@@ -4,6 +4,7 @@ import numpy as np
 import subprocess
 import os
 import sys
+from sklearn.neural_network import MLPClassifier
 
 if not os.path.exists(os.path.join('.', 'data')):
     os.mkdir(os.path.join('.', 'data'))
@@ -22,8 +23,33 @@ y_test = np.load(os.path.join('data', 'y_test.npy'))
 
 NN = NeuralNetwork()
 
-NN.train(x_train, y_train, 10)
-input_pred = np.array([1,1,2,2,3,4404,0,4,3,1])
+NN.train(x_train, y_train, 1)
+print(x_test[0])
+print(x_test[0].shape)
+print(y_test[0])
+input_pred = np.array(x_test[0])
 print(NN.predict(input_pred))
 NN.view_error_development()
-# NN.test_evaluation(input_test_scaled, output_test_scaled)
+NN.test_evaluation(x_test, y_test)
+
+clf = MLPClassifier(
+    activation='relu',
+    solver='sgd',
+    alpha=1e-5,
+    hidden_layer_sizes=(34),
+    random_state=1,
+    max_iter=5000
+)
+clf.fit(x_train, y_train)
+
+print( 'accuracy: ',clf.score( x_test, y_test ))
+
+pred = clf.predict(x_test)
+print(pred)
+print(y_test)
+sum = 0
+for i in range(len(pred)):
+    if pred[i] == y_test[i]:
+        sum += 1
+
+print(f'{sum} / {len(pred)}')

simplennet/neural_network.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class NeuralNetwork():
     def __init__(self, ):
-        self.inputSize = 10
+        self.inputSize = 34
         self.outputSize = 1
-        self.hiddenSize = 5
+        self.hiddenSize = 34
 
         self.W1 = np.random.rand(self.inputSize, self.hiddenSize)
         self.W2 = np.random.rand(self.hiddenSize, self.outputSize)
@@ -31,7 +32,7 @@ class NeuralNetwork():
         return s * (1 - s)
 
     def backward(self, X, y, o):
-        self.o_error = y - o
+        self.o_error = o - y
         self.o_delta = self.o_error * self.sigmoidPrime(o)
         self.z2_error = np.matmul(self.o_delta, np.matrix.transpose(self.W2))
         self.z2_delta = self.z2_error * self.sigmoidPrime(self.z2)
@@ -39,8 +40,12 @@ class NeuralNetwork():
         self.W2 += np.matmul(np.matrix.transpose(self.z2), self.o_delta)
 
     def train(self, X, y, epochs):
+        y = np.array([[i] for i in y])
+
         for epoch in range(epochs):
             o = self.forward(X)
+            j = (-1/y.shape[0]) * np.sum(np.multiply(y, np.log(o)) + np.multiply((1 - y), np.log(1 - o)))
+            print(j)
             self.backward(X, y, o)
             self.error_list.append(np.abs(self.o_error).mean())
 
@@ -55,16 +60,33 @@ class NeuralNetwork():
         plt.show()
 
     def test_evaluation(self, input_test, output_test):
-        for i, test_element in enumerate(input_test):
-            if self.predict(test_element) > self.limit and output_test[i] == 1:
-                self.true_positives += 1
-            if self.predict(test_element) < self.limit and output_test[i] == 1:
-                self.false_negatives += 1
-            if self.predict(test_element) > self.limit and output_test[i] == 0:
-                self.false_positives += 1
-            if self.predict(test_element) < self.limit and output_test[i] == 0:
-                self.true_negatives += 1
-        print('True positives: ', self.true_positives, '\nTrue negatives: ', self.true_negatives,
-              '\nFalse positives: ', self.false_positives, '\nFalse negatives: ', self.false_negatives,
-              '\nAccuracy: ', (self.true_positives + self.true_negatives) /
-              (self.true_positives + self.true_negatives + self.false_positives + self.false_negatives))
+        # for i, test_element in enumerate(input_test):
+        #     if self.predict(test_element) > self.limit and output_test[i] == 1:
+        #         self.true_positives += 1
+        #     if self.predict(test_element) < self.limit and output_test[i] == 1:
+        #         self.false_negatives += 1
+        #     if self.predict(test_element) > self.limit and output_test[i] == 0:
+        #         self.false_positives += 1
+        #     if self.predict(test_element) < self.limit and output_test[i] == 0:
+        #         self.true_negatives += 1
+        #
+        #     # print(f'{self.predict(test_element)} ---- {output_test[i]}')
+        # print('True positives: ', self.true_positives, '\nTrue negatives: ', self.true_negatives,
+        #       '\nFalse positives: ', self.false_positives, '\nFalse negatives: ', self.false_negatives,
+        #       '\nAccuracy: ', (self.true_positives + self.true_negatives) /
+        #       (self.true_positives + self.true_negatives + self.false_positives + self.false_negatives))
+        print('----- MANUAL TESTING -----')
+        sum = 0
+        preds = []
+        for test_element in input_test:
+            pred = self.predict(test_element)
+            preds.append(pred)
+
+        for i in range(len(preds)):
+            if preds[i] == output_test[i]:
+                sum += 1
+
+        print(preds)
+        print(output_test)
+
+        print(f'{sum} / {len(preds)}')

simplennet/prepare_data.py
@@ -6,9 +6,10 @@ import os
 import matplotlib.pyplot as plt
 
 OUTPUT_COLUMN = "WorkLifeBalance"
-COLUMNS = ["WorkLifeBalance", "Department", "Gender", "JobLevel", "JobRole", "JobSatisfaction", "MonthlyIncome",
-           "OverTime", "YearsAtCompany", "YearsInCurrentRole", "YearsSinceLastPromotion"]
-data = pd.read_csv('data.csv', usecols=COLUMNS)
+# COLUMNS = ["WorkLifeBalance", "Department", "Gender", "JobLevel", "JobRole", "JobSatisfaction", "MonthlyIncome",
+#            "OverTime", "YearsAtCompany", "YearsInCurrentRole", "YearsSinceLastPromotion", "Education", "DistanceFromHome", "TotalWorkingYears"]
+# data = pd.read_csv('data.csv', usecols=COLUMNS)
+data = pd.read_csv('data.csv')
 columns_in_order = data.columns.values
 
 print('Discretizar valores de output a 0 - 1...')
@@ -64,9 +65,9 @@ x_test, x_cv, y_test, y_cv = train_test_split(
 print(x_test.values.shape)
 print(y_test.values.shape)
 
-# output = np.array([[x] for x in df.loc[:,output_column].to_list()])
-# df = df.drop(output_column, axis=1)
-# columns_in_order = np.delete(columns_in_order, np.where(columns_in_order == output_column))
+# output = np.array([[x] for x in df.loc[:,OUTPUT_COLUMN].to_list()])
+# df = df.drop(OUTPUT_COLUMN, axis=1)
+# columns_in_order = np.delete(columns_in_order, np.where(columns_in_order == OUTPUT_COLUMN))
 # input = df[columns_in_order].values
 
 np.save(os.path.join('data', 'x_train.npy'), x_train)